9479bf6bf4e043b56b0d8611fece57a21cf84acc
yrolland <yann.rolland@iadvize.com>
2021-01-24 18:21:31 +0100

    cleanup

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
-function state(rows){
-    return new State(rows)
+const MIN_CAN_TAKE = 1
+const MAX_CAN_TAKE = 3
+const WINNING_STRAT_BLOCK_SIZE = 5
+const STARTING_PIECES_COUNT = 20
+const SAMPLE_SIZE = 100
+
+const config = {
+    iterations: 200000,
+    binaryThresh: 0.5,
+    hiddenLayers: [10],
+    log: true,
+    logPeriod: 1000,
+    learningRate: 0.1,
+    activation: 'sigmoid',
+    errorThresh: 0.02,
 }
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min)) + min;
-  }
-
-function solve(n){
-    return constrain((n-1) % 4, 1, 3)
 }
 
-function encode(i){
-    return i.toString(2).padStart(5,"0").split("").map(e => +e)
+function solve(n) {
+    return constrain((n - 1) % WINNING_STRAT_BLOCK_SIZE, MIN_CAN_TAKE, MAX_CAN_TAKE)
 }
 
-function normalize(v){
-    return v / 20
+function normalize(v) {
+    return v / STARTING_PIECES_COUNT
 }
 
-function generateSample(){
-    let input = getRandomInt(1,20+1)
+function generateSample() {
+    let input = getRandomInt(1, STARTING_PIECES_COUNT + 1)
     return [input, solve(input)]
 }
 
-function constrain(v,min,max){
-    return Math.max(Math.min(max,v), min)
+function constrain(v, min, max) {
+    return Math.max(Math.min(max, v), min)
 }
 
-function formatSample(io){
+function formatSample(io) {
     let [input, output] = io
     return {
         raw: input,
@@ -38,12 +47,11 @@ function formatSample(io){
     }
 }
 
-function bestLabel(output){
+function bestLabel(output) {
     let bestScore = -1
-    let bestLabel = null 
-    for(k in output){
-        // console.log(`${k}:${output[k]}`)
-        if((bestScore == -1) || (output[k] > bestScore)){
+    let bestLabel = null
+    for (k in output) {
+        if ((bestScore == -1) || (output[k] > bestScore)) {
             bestScore = output[k]
             bestLabel = k
         }
@@ -53,32 +61,17 @@ function bestLabel(output){
 
 window.onload = function () {
 
-    const config = {
-        iterations: 200000,
-        binaryThresh: 0.5,
-        hiddenLayers: [10],
-        log: true,
-        logPeriod: 1000,
-        learningRate: 0.1,
-        activation: 'sigmoid',
-        errorThresh: 0.02,
-    };
-
     // create a simple feed forward neural network with backpropagation
     const net = new brain.NeuralNetwork(config);
 
     let trainingData = []
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < SAMPLE_SIZE; i++) {
         trainingData.push(formatSample(generateSample()))
     }
 
-    // console.log(JSON.stringify(trainingData))
-
     console.log(net.train(trainingData))
 
-    console.log('validation:')
-
-    for (let index = 1; index <= 20; index++) {
+    for (let index = 1; index <= STARTING_PIECES_COUNT; index++) {
         let prediction = net.run([normalize(index)])
         console.log(`for input ${index}, predicted ${bestLabel(prediction)}, correct was ${solve(index)}.\tDetails are ${JSON.stringify(prediction)}`)
     }
