03b03fbb12f1612042814f64fdd303950639665c
yrolland <yann.rolland@iadvize.com>
2021-01-31 01:44:48 +0100

    wip

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ import { randomPlay } from './randomStrategy.js'
 
 window.onload = function () {
     let predictNN = trainNN()
+    let ga = playGA()
+
     let wrNN = winrate(predictNN, randomPlay)
     console.log(`NN vs random winrate: ${wrNN * 100}%`)
 
-    playGA()
+    let wrGA = winrate(ga, randomPlay)
+    console.log(`GA vs random winrate: ${wrGA * 100}%`)    
 }
